e768370a652567e51a7c72b6572e5798d8ce046c
Harry-KNIGHT <eliott.knight@icloud.com>
2023-04-26 21:27:53 +0200

    Update lyrics array

--- PlayerMusicLyricAnimationPOC/ContentView.swift
+++ PlayerMusicLyricAnimationPOC/ContentView.swift
@@ -79,14 +79,16 @@ extension Music {
 		cover: "eminemCover",
 		title: "The Real Slim Shady",
 		lyrics:
-				[
-					"May I have your attention, please ?",
-					"May I have your attention, please ?\n Will the real Slim Shady please stand up ?",
-					"I repeat, will the real Slim Shady please stand up ?\n We're gonna have a problem here...",
-					"For I have your attention, please ?",
-					"May I have your attention, please ?\n Will the real Slim Shady please stand up ?",
-					"I repeat, will the real Slim Shady please stand up ?\n We're gonna have a problem here...",
-				]
+			[
+				"May I have your attention, please ?",
+				"May I have your attention, please ?\n Will the real Slim Shady please stand up ?",
+				"I repeat, will the real Slim Shady please stand up ?",
+				"We're gonna have a problem here...",
+				"May I have your attention, please ?",
+				"May I have your attention, please ?\n Will the real Slim Shady please stand up ?",
+				"I repeat, will the real Slim Shady please stand up ?",
+				"We're gonna have a problem here..."
+			]
 	)
 }
 
