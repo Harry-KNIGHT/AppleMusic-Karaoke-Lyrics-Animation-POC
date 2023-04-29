159d1121199393d97f0287d87d263a4a3aa15215
Harry-KNIGHT <eliott.knight@icloud.com>
2023-04-29 11:35:03 +0200

    Renaming and move backgroud color on it's own view

--- PlayerMusicLyricAnimationPOC/Views/Screen/LyricsPlayerView.swift
+++ PlayerMusicLyricAnimationPOC/Views/Screen/LyricsPlayerView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  LyricsPlayerView.swift
 //  PlayerMusicLyricAnimationPOC
 //
 //  Created by Elliot Knight on 25/04/2023.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LyricsPlayerView: View {
 	let music: Music
 	@State private var count = 0
 	let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
@@ -15,15 +15,8 @@ struct ContentView: View {
 
     var body: some View {
 		ZStack {
-			LinearGradient(
-				colors: [
-					Color(red: 28/255, green: 28/255, blue: 28/255),
-					Color(red: 43/255, green: 43/255, blue: 43/255),
-					Color(red: 71/255, green: 71/255, blue: 71/255)],
-				startPoint: .topLeading,
-				endPoint: .bottomTrailing
-			)
-			.edgesIgnoringSafeArea(.all)
+			PlayerBackgroundColor()
+				.edgesIgnoringSafeArea(.all)
 
 			VStack(alignment: .leading, spacing: 0) {
 				HStack {
@@ -79,9 +72,9 @@ struct ContentView: View {
 
 // MARK: Preview
 
-struct ContentView_Previews: PreviewProvider {
+struct LyricsPlayerView_Previews: PreviewProvider {
     static var previews: some View {
-		ContentView(music: .musicSample)
+		LyricsPlayerView(music: .musicSample)
 
     }
 }
