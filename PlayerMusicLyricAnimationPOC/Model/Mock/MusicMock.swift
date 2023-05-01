cb2d7da306e0374c0a9fe822496f2c44296a010c
Harry-KNIGHT <eliott.knight@icloud.com>
2023-05-01 10:19:14 +0200

    Feat: Add lyrics on mock

--- PlayerMusicLyricAnimationPOC/Model/Mock/MusicMock.swift
+++ PlayerMusicLyricAnimationPOC/Model/Mock/MusicMock.swift
@@ -21,11 +21,14 @@ extension Music {
 				"Will the real Slim Shady please stand up ?",
 				"I repeat, will the real Slim Shady please stand up ?",
 				"We're gonna have a problem here...",
-				"May I have your attention, please ?",
-				"May I have your attention, please ?",
-				"Will the real Slim Shady please stand up ?",
-				"I repeat, will the real Slim Shady please stand up ?",
-				"We're gonna have a problem here..."
+				"Y'all act like you never seen a white person before",
+				"Jaws all on the floor like Pam, like Tommy just burst in the door",
+				"And started whoopin' her ass worse than before",
+				"They first were divorced, throwin' her over furniture (ah!)",
+				"It's the return of the-, oh, wait, no way, you're kidding",
+				"He didn't just say what I think he did, did he?",
+				"And Dr. Dre said-, nothing you idiots",
+				"Dr. Dre's dead, he's locked in my basement"
 			]
 	)
 }
